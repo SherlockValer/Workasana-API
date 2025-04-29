24ef8466dc316df7a06920fcdc33d7d8d42c35c3
Vaibhav Chopde <vchopde77@gmail.com>
2025-04-29 19:14:15 +0530

    fix: Fixed Project Routes

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,5 +1,7 @@
 const { catchAsync } = require("../services/catchAsync");
 const Project = require("../models/project.model");
+const Task = require("../models/task.model");
+const User = require("../models/user.model");
 
 // Add a new project
 exports.addProject = catchAsync(async (req, res) => {
@@ -22,16 +24,78 @@ exports.addProject = catchAsync(async (req, res) => {
   }
 });
 
-// get all projects
+// get all projects with data
 exports.getProjects = catchAsync(async (req, res) => {
+  const projects = await Task.aggregate([
+    {
+      $group: {
+        _id: "$project",
+        tasks: {
+          $push: {
+            _id: "$_id",
+            name: "$name",
+            status: "$status",
+            owners: "$owners",
+            tags: "$tags",
+            timeToComplete: "$timeToComplete",
+            createdAt: "$createdAt",
+          },
+        },
+        totalTasks: { $count: {} },
+        completedTasks: {
+          $sum: {
+            $cond: { if: { $eq: ["$status", "Completed"] }, then: 1, else: 0 },
+          },
+        }, // Count completed tasks
+      },
+    },
+    {
+      $addFields: {
+        status: {
+          $cond: {
+            if: { $eq: ["$completedTasks", "$totalTasks"] },
+            then: "Completed",
+            else: "In Progress",
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        project: "$_id",
+        tasks: 1,
+        totalTasks: 1,
+        status: 1,
+      },
+    },
+  ]);
+
+  if (projects) {
+    const populatedData = await Project.populate(projects, {
+      path: "project",
+      select: "name description",
+    });
+
+    const populatedDataWithUser = await User.populate(populatedData, {
+      path: "tasks.owners",
+      select: "name",
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: populatedDataWithUser,
+    });
+  }
+});
+
+// get projects only
+exports.getProjectsOnly = catchAsync(async (req, res) => {
   const projects = await Project.find();
 
   if (projects) {
     res.status(200).json({
-      status: "success",
-      data: {
-        projects,
-      },
+      projects,
     });
   }
 });
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { addProject, getProjects } = require("../controllers/projectController");
+const {
+  addProject,
+  getProjects,
+  getProjectsOnly,
+} = require("../controllers/projectController");
 const { protect } = require("../controllers/authController");
 const router = express.Router();
 
 // Add a new project
 router.post("/", protect, addProject);
 
-// get all projects
+// get all projects with data
 router.get("/", getProjects);
+
+// get all projects only
+router.get("/only", getProjectsOnly);
 
 module.exports = router;
