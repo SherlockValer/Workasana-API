f3d8e205a9f5be36499fcb740206db57a81267c3
Vaibhav Chopde <vchopde77@gmail.com>
2025-04-29 19:16:01 +0530

    chore: minor changes in tag model

--- models/tag.model.js
+++ models/tag.model.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 // Tag Schema
 const tagSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true }, // Tag names must be unique
+  color: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Tag", tagSchema);
+const Tag = mongoose.model("Tag", tagSchema);
+
+module.exports = Tag;
