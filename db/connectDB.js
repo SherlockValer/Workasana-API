7ac39d5031d78706bdfdb7176e30c0f4f7533365
Vaibhav Chopde <vchopde77@gmail.com>
2025-05-30 19:23:43 +0530

    chore: minor fixes

--- db/connectDB.js
+++ db/connectDB.js
@@ -4,11 +4,15 @@ require("dotenv").config();
 
 const workasanaURI = process.env.MONGODB;
 
-mongoose.set("bufferCommands", false);
-
 const connectDB = async () => {
   await mongoose
-    .connect(workasanaURI, { dbName: "workasana" })
+    .connect(workasanaURI, {
+      dbName: "workasana",
+      serverSelectionTimeoutMS: 30000, // 30 seconds
+      socketTimeoutMS: 45000, // 45 seconds
+      bufferMaxEntries: 0,
+      bufferCommands: false,
+    })
     .then(() => console.log("Connected to Database."))
     .catch((error) => console.log(error));
 };
